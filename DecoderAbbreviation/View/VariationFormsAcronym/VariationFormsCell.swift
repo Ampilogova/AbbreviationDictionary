3edce7163b57617d308614ae710ab11d0a77f0ca
Tatiana Ampilogova <53023750+Ampilogova@users.noreply.github.com>
2022-08-02 09:52:51 -0700

    fix variations cell

---
+++ DecoderAbbreviation/View/VariationFormsAcronym/VariationFormsCell.swift
@@ -0,0 +1,48 @@
+//
+//  VariationFormsCell.swift
+//  DecoderAbbreviation
+//
+//  Created by Tatiana Ampilogova on 8/2/22.
+//
+
+import UIKit
+
+class VariationFormsCell: UITableViewCell {
+    
+    private let acronymLabel: UILabel = {
+        var label = UILabel()
+        label.font = .systemFont(ofSize: 15, weight: .semibold)
+        return label
+    }()
+    
+    private let acronymDetailsLabel: UILabel = {
+        var label = UILabel()
+        label.font = .systemFont(ofSize: 14)
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        acronymLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.contentView.addSubview(acronymLabel)
+        NSLayoutConstraint.activate([
+            acronymLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            acronymLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+        ])
+        
+        acronymDetailsLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.contentView.addSubview(acronymDetailsLabel)
+        NSLayoutConstraint.activate([
+            acronymDetailsLabel.topAnchor.constraint(equalTo: acronymLabel.bottomAnchor,constant: 5),
+            acronymDetailsLabel.leadingAnchor.constraint(equalTo: acronymLabel.leadingAnchor),
+            acronymDetailsLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10),
+        ])
+    }
+    
+    func configure(with model: VariationForms) {
+        self.acronymLabel.text = model.fullForms
+        self.acronymDetailsLabel.text = String(model.frequently) + " since " + String(model.date)
+    }
+    
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+}
